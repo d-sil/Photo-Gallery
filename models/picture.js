336e51888c6abc7977122d92477adb70becdc45e
David Silva <dsilva23@yahoo.com>
2018-05-04 22:24:15 +0000

    change image resolution

--- models/picture.js
+++ models/picture.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 
 var PictureSchema = new mongoose.Schema({
     image: String,
+    anchor: String
 });
 
 module.exports = mongoose.model("Picture", PictureSchema);
